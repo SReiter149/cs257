399ba75f758e2bbfb67a5e5452c85bac7850ef31
Samuel Reiter <samreiter9@gmail.com>
2025-04-18 08:45:11 -0500

    removing pandas dependency

--- individual/cli.py
+++ individual/cli.py
@@ -7,7 +7,7 @@ DESCRIPTION: Shows a list of all the songs in the album, case sensitive
 """
 
 import sys
-import pandas as pd
+import csv
 import os
 import pdb
 import argparse
@@ -25,23 +25,19 @@ def usage_statement():
     statement += 'returns the songs in the album'
     return statement
 
-def readcsv():
-    songs = pd.read_csv("../data/songs.csv")
+def getAlbum(album):
+    songs = []
+    with open('../data/songs.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row["album"] == album:
+                songs.append(row)
     return songs
 
-def getAlbum(album, songs):
-    if album in set(songs["album"].to_list()):
-        albumSongs = songs[songs["album"] == album]["title"].to_list()
-    else:
-        return "album not found, try a new album name"
-    # pdb.set_trace()
-    return albumSongs
-
 def main():
-    allsongs = readcsv()
     arguments = get_parsed_arguments()
     for album in arguments.album:
-        songs = getAlbum(album, allsongs)
+        songs = getAlbum(album)
         print(songs)
 
 if __name__ == "__main__":
