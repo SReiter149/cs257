4b6789344922d78629ebabc1c85d8fa4d8dbf0e2
Samuel Reiter <samreiter9@gmail.com>
2025-04-17 22:28:25 -0500

    I need to read the instructions lol :)

--- individual/cli.py
+++ individual/cli.py
@@ -1,17 +1,24 @@
 """
 cli.py
+Sam Reiter 4/17/2025
 NAME: cli.py - command-line interface exercise
-SYNOPSIS: python3 cli.py "album name"
+SYNOPSIS: python3 cli.py -a "album name"
 DESCRIPTION: Shows a list of all the songs in the album, case sensitive
 """
-
 
 import sys
 import pandas as pd
 import os
 import pdb
+import argparse
 
 DIRECTORY = os.getcwd()
+
+def get_parsed_arguments():
+    parser = argparse.ArgumentParser(description="Get the list of songs in the album")
+    parser.add_argument("--album", "-a", help="name of album (case sensitive)", nargs = "+")
+    parsed_arguments = parser.parse_args()
+    return parsed_arguments
 
 def usage_statement():
     statement = f'Usage: {sys.argv[0]} "album name"\n'
@@ -22,17 +29,20 @@ def readcsv():
     songs = pd.read_csv("../data/songs.csv")
     return songs
 
-def getAlbum(songs):
-    albumSearch = sys.argv[1]
-    if albumSearch in set(songs["album"].to_list()):
-        albumSongs = songs[songs["album"] == albumSearch]["title"].to_list()
+def getAlbum(album, songs):
+    if album in set(songs["album"].to_list()):
+        albumSongs = songs[songs["album"] == album]["title"].to_list()
     else:
         return "album not found, try a new album name"
     # pdb.set_trace()
     return albumSongs
 
+def main():
+    allsongs = readcsv()
+    arguments = get_parsed_arguments()
+    for album in arguments.album:
+        songs = getAlbum(album, allsongs)
+        print(songs)
 
 if __name__ == "__main__":
-    songs = readcsv()
-    print(getAlbum(songs))
-
+    main()
